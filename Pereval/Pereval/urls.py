a9240f9a0d4991af4d9dc0bed4fd2d035eab1a9c
Константин Нехорошев <nekhoroshevkg@russiancrab.ru>
2023-08-14 14:44:29 +1000

    url до приложения

--- Pereval/Pereval/urls.py
+++ Pereval/Pereval/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('swagger-ui/', TemplateView.as_view(template_name='swagger-ui.html'), name='swagger-ui'),
+    path('', include('add_data.urls')),
 ]
 
 schema_view = get_schema_view(
