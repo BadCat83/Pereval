4ab966b0cadea12aa9b954ca2eb631259cd5daf9
Константин Нехорошев <nekhoroshevkg@russiancrab.ru>
2023-08-14 14:03:18 +1000

    Почистил

--- Pereval/Pereval/urls.py
+++ Pereval/Pereval/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('swagger-ui/', TemplateView.as_view(template_name='swagger-ui.html'), name='swagger-ui'),
-    path('api/', include('mountain_pass.urls')),
 ]
 
 schema_view = get_schema_view(
